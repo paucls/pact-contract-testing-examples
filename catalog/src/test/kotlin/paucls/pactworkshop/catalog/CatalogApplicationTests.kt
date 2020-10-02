7a5b082f8b0088516efea34d107aa4a0257d0978
Paulo Clavijo Esteban <paucls@gmail.com>
2020-10-02 15:39:37 +0100

    Generated a catalog spring boot service.

---
+++ catalog/src/test/kotlin/paucls/pactworkshop/catalog/CatalogApplicationTests.kt
@@ -0,0 +1,13 @@
+package paucls.pactworkshop.catalog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
