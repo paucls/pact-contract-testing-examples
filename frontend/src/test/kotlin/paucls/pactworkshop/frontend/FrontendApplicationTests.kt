d97ff4de34c6fa5a0e5684a8c1a45a17673d91ef
Paulo Clavijo Esteban <paucls@gmail.com>
2020-10-05 22:09:54 +0100

    Generated a fronted spring boot mvc app.

---
+++ frontend/src/test/kotlin/paucls/pactworkshop/frontend/FrontendApplicationTests.kt
@@ -0,0 +1,13 @@
+package paucls.pactworkshop.frontend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
