cc242ac2d33520c06277942a9995193ff795b541
Paulo Clavijo Esteban <paucls@gmail.com>
2020-10-05 22:46:48 +0100

    frontend - added bootstrap

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.webjars:bootstrap:4.5.2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
