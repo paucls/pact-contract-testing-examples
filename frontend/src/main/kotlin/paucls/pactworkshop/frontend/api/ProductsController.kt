4931e4b85354b7c0d2608af0197977802b8f0b11
Paulo Clavijo Esteban <paucls@gmail.com>
2020-10-06 09:32:47 +0100

    front-end - implemented product details page

--- frontend/src/main/kotlin/paucls/pactworkshop/frontend/api/ProductsController.kt
+++ frontend/src/main/kotlin/paucls/pactworkshop/frontend/api/ProductsController.kt
@@ -3,24 +3,29 @@ package paucls.pactworkshop.frontend.api
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import java.util.*
+import org.springframework.web.bind.annotation.PathVariable
 
 
 @Controller
 class ProductsController {
 
+    private val products = listOf(
+            Product(id = 0, name = "Gem Visa", type = "CREDIT_CARD"),
+            Product(id = 1, name = "28 Degrees", type = "CREDIT_CARD"),
+            Product(id = 2, name = "MyFlexiPay", type = "PERSONAL_LOAN")
+    )
+
     @GetMapping("/")
     fun greeting(model: Model): String {
-        val products = listOf(
-                Product(name = "Gem Visa", type = "CREDIT_CARD"),
-                Product(name = "28 Degrees", type = "CREDIT_CARD"),
-                Product(name = "MyFlexiPay", type = "PERSONAL_LOAN")
-        )
         model.addAttribute("products", products)
-
         return "products"
     }
 
+    @GetMapping("/products/{id}")
+    fun productDetails(@PathVariable("id") id: Int, model: Model): String {
+        model.addAttribute("product", products[id])
+        return "product-details"
+    }
 }
 
-data class Product(val id: UUID = UUID.randomUUID(), val name: String, val type: String)
+data class Product(val id: Int, val name: String, val type: String)
