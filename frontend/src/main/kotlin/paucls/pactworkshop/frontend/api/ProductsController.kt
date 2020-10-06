60ab604fab83e14f8b3a31899ef47ba923363bb9
Paulo Clavijo Esteban <paucls@gmail.com>
2020-10-06 09:05:29 +0100

    front-end implemented stubbed products list

--- frontend/src/main/kotlin/paucls/pactworkshop/frontend/api/ProductsController.kt
+++ frontend/src/main/kotlin/paucls/pactworkshop/frontend/api/ProductsController.kt
@@ -3,6 +3,7 @@ package paucls.pactworkshop.frontend.api
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import java.util.*
 
 
 @Controller
@@ -10,7 +11,16 @@ class ProductsController {
 
     @GetMapping("/")
     fun greeting(model: Model): String {
+        val products = listOf(
+                Product(name = "Gem Visa", type = "CREDIT_CARD"),
+                Product(name = "28 Degrees", type = "CREDIT_CARD"),
+                Product(name = "MyFlexiPay", type = "PERSONAL_LOAN")
+        )
+        model.addAttribute("products", products)
+
         return "products"
     }
 
 }
+
+data class Product(val id: UUID = UUID.randomUUID(), val name: String, val type: String)
