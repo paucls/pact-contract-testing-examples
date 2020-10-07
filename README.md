0618bb98a8dadad7331a8105b380416f88d8b210
Paulo Clavijo Esteban <paucls@gmail.com>
2020-10-07 10:39:26 +0100

    frontend - Adding first consumer contract for the get all products interaction.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Pact Workshop
+
+## References
+https://docs.pact.io/implementation_guides/jvm/consumer/junit5
+https://kreuzwerker.de/post/migrating-pact-contract-tests-from-junit4-to-junit5
+https://github.com/tinexw/cdc-with-pact/tree/junit5
---
+++ frontend/src/test/kotlin/paucls/pactworkshop/frontend/CatalogClientConsumerPactTest.kt
@@ -0,0 +1,60 @@
+package paucls.pactworkshop.frontend
+
+import au.com.dius.pact.consumer.MockServer
+import au.com.dius.pact.consumer.dsl.PactDslJsonArray
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt
+import au.com.dius.pact.consumer.junit5.PactTestFor
+import au.com.dius.pact.core.model.RequestResponsePact
+import au.com.dius.pact.core.model.annotations.Pact
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import paucls.pactworkshop.frontend.client.CatalogClient
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class, PactConsumerTestExt::class)
+@PactTestFor(providerName = "catalog", port = "8081")
+class CatalogClientConsumerPactTest {
+
+    @Autowired
+    private lateinit var catalogClient: CatalogClient
+
+    @Pact(consumer = "frontend", provider = "catalog")
+    fun pact_get_all_products_when_products_exist(builder: PactDslWithProvider): RequestResponsePact {
+        val headers = mapOf("Content-Type" to "application/json")
+
+        val responseBody = PactDslJsonArray
+                .arrayEachLike()
+                .stringType("id", "123")
+                .stringType("name", "Mastercard")
+                .stringType("type", "CREDIT_CARD")
+                .close()
+
+        return builder
+                .given("products exist")
+                .uponReceiving("a request to get all products")
+                .method("GET")
+                .matchPath("/products")
+                .willRespondWith()
+                .status(200)
+                .headers(headers)
+                .body(responseBody)
+                .toPact()
+    }
+
+    @PactTestFor(pactMethod = "pact_get_all_products_when_products_exist")
+    @Test
+    fun get_all_products_when_products_exist(mockServer: MockServer) {
+        val products = catalogClient.getAllProducts()
+
+        assertThat(products).hasSize(1)
+        assertThat(products[0].id).isEqualTo(123)
+        assertThat(products[0].name).isEqualTo("Mastercard")
+        assertThat(products[0].type).isEqualTo("CREDIT_CARD")
+    }
+}
